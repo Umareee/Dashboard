eb0339bf11e836880b35eddb823057761e9029c5
Umar Afzal Khan <umersaad9222@gmail.com>
2025-07-30 17:46:35 +0500

    first

---
+++ src/icons/index.tsx
@@ -0,0 +1,19 @@
+import ArrowUpIcon from "./arrow-up.svg";
+import ArrowDownIcon from "./arrow-down.svg";
+import GridIcon from "./grid.svg";
+import GroupIcon from "./group.svg";
+import BoxIconLine from "./box-line.svg";
+import ChevronDownIcon from "./chevron-down.svg";
+import HorizontaLDots from "./horizontal-dots.svg";
+import MoreDotIcon from "./more-dot.svg";
+
+export {
+  MoreDotIcon,
+  GridIcon,
+  ArrowUpIcon,
+  ArrowDownIcon,
+  GroupIcon,
+  BoxIconLine,
+  ChevronDownIcon,
+  HorizontaLDots,
+};
